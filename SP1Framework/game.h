7e78c91b5ecfefe9beebfeff067206f48b173ee0
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2018-08-15 16:46:09 +0800

    BANNER

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -7,6 +7,7 @@
 #include "level.h"
 #include "entity.h"
 #include "fileio.h"
+#include "menu.h"
 #include "definitions.h"
 #include <string>
 #include <cstdlib>
@@ -36,6 +37,7 @@ enum EGAMESTATES
 {
     S_SPLASHSCREEN,
 	S_MENU,
+	S_OPTIONS,
     S_GAME,
     S_COUNT
 };
@@ -46,19 +48,31 @@ void update      ( CStopWatch * timer, double missedTime ); // update the game a
 void render      ( CStopWatch * timer );      // renders the current state of the game to the console
 void shutdown    ( void );      // do clean up, free memory
 
+/*
+	THE UPDATE FUNCTIONS
+*/
 
 void splashScreenWait();	// waits for time to pass in splash screen
 void mainMenu();			// main menu logic
-void gameplay();			// gameplay logic
-void moveCharacter();		// moves the character, collision detection, physics, etc
 void playerShoot();			// allows the player to shoot
+void gameplay();			// gameplay logic
+void menuNavigate();		// Navigation of menu
 void resetLevel(int floor); // Generate a new map, with input floor for what to put inside the rooms
+void controlPlayer();		// moves the character, collision detection, physics, etc
 void processUserInput();	// checks if you should change states or do something else with the game, e.g. pause, exit
 void clearScreen();			// clears the current screen and draw from scratch 
+
+/*
+	THE RENDER FUNCTIONS
+*/
+
 void renderSplashScreen();	// renders the splash screen
 void renderMainMenu();		// renders the main menu
 void renderGame();			// renders the game stuff
-void renderMap();			// renders the map to the buffer first
+void renderTitle();
+void renderMenu();
+void renderScore();			// render the current score from
+void renderCursor();
 void renderCharacter();		// renders the character into the buffer
 void renderFramerate();		// renders debug information, frame rate, elapsed time, etc
 void renderToScreen();		// dump the contents of the buffer to the screen, one frame worth of game
@@ -66,6 +80,5 @@ void renderLevel();			// renders the borders of the rooms
 void renderPellets();		// render pellets to buffer
 void renderEnemy(); // renders all enemies to the buffer
 void renderStat();   //renders the player stats on the right side of the console
-void renderScore();			// render the current score from
 
 #endif // _GAME_H
---
+++ SP1Framework/menu.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef _MENU_H
+#define _MENU_H
+#include "Framework/console.h"
+
+
+#endif // !_MENU_H
